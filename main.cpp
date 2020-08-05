5095ce588473d15366f7e9e4c02c670e37f66de0
Daniel Guachamin <guillermo.guachamin@epn.edu.ec>
2020-08-05 21:00:06 +0000

    Intente agregar la funcion ios::app, pero me da un error que aparentemente no debería salir

--- main.cpp
+++ main.cpp
@@ -3,8 +3,9 @@
 //LIBRERIA PARA TRABAJAR CON ARCHIVOS
 #include <fstream>
 
-void Ingresar_info();
-void Mostrrar_info();
+void Crear_info();
+void Añadir_info();
+void Mostrar_info();
 int menu();
 
 using namespace std;
@@ -18,38 +19,45 @@ int main() {
       case 1:
       {
         cin.ignore();
-        Ingresar_info();
+        Crear_info();
       break;
       }
       case 2:
       {
-        Mostrrar_info();
+        cin.ignore();
+        Añadir_info();
       break;
       }
+      case 3:
+      {
+        Mostrar_info();
+      break;
     }
-  } while (opcion != 3);
+  } while (opcion != 4);
   cout << "\nGracias por usar nuestro servicio" << endl;
   return 0;
 }
+
 int menu()
 {
   int op;
   do
   {
     cout << "\nMENU DE OPCIONES" << endl;
-    cout << "1. Ingresar un nuevo contacto" << endl;
-    cout << "2. Mostrar todos los contactos" << endl;
-    cout << "3. Salir" << endl;
+    cout << "1. Crear un registro de contactos" << endl;
+    cout << "2. Añadir otro contacto" << endl;
+    cout << "3. Mostrar todos los contactos" << endl;
+    cout << "4. Salir" << endl;
     cout << "Eliga una opcion: ";
     cin >> op;
-    if ((op < 1) || (op > 3))
+    if ((op < 1) || (op > 4))
     {
       cout << "\n¡Ingrese una opción correcta!" << endl;
     }
-  } while ((op < 1) || (op > 3));
+  }while((op < 1) || (op > 4));
   return op;
 }
-void Ingresar_info()
+void Crear_info()
 {
   string nombre, apellido;
   int edad;
@@ -79,7 +87,37 @@ void Ingresar_info()
 
   archivoPrueba.close();
 }
-void Mostrrar_info()
+void Añadir_info()
+{
+  string nombre, apellido;
+  int edad;
+  char r;
+  ofstream archivoPrueba;
+  string nombreArchivo;
+  cout << "\nIngrese el nombre del archivo: ";
+  getline(cin, nombreArchivo);
+
+  archivoPrueba.open(nombreArchivo.c_str(), ios::app);
+
+  do
+  {
+    cout << "Ingrese el nombre: ";
+    getline(cin, nombre, '\n');
+    cout << "Ingrese el apellido: ";
+    getline(cin, apellido, '\n');
+    cout << "Ingresa tu edad: ";
+    cin >> edad;
+    archivoPrueba << nombre << " " << apellido << " " << edad << '\n';
+
+    cout << "\nDesea agregar otro contacto s/n ";
+    cin >> r;
+    cin.ignore();
+
+  }while ( r == 's');
+
+  archivoPrueba.close();
+}
+void Mostrar_info()
 {
   string nombre, apellido;
   int edad;
@@ -99,4 +137,5 @@ void Mostrrar_info()
     }
   }
   archivoLectura.close();
+}
 }
